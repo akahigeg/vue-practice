2debebe5463ba3ef170a669917903ac149be49db
akahige <akahigeg@gmail.com>
2020-08-27 14:07:42 +0900

    SPAのルーティングのサンプル

---
+++ spa-sample/src/components/page1.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <p>This is Page 1</p>
+    <router-link to="/page2">Go to Page 2</router-link>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
---
+++ spa-sample/src/components/page2.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <p>This is Page 2</p>
+    <router-link to="/">Go to Page 1</router-link>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
